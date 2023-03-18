0daddc51bf1aa749f055f4481585d6c3703f49f7
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2023-03-18 19:42:57 +0200

    blah

---
+++ bc/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+
+class BC
+{
+    public static void Main()
+    {
+        Console.WriteLine("bc clone");
+        Console.WriteLine("Input number 1:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Now input number 2:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Add or subtract? [asq]");
+
+        switch (Console.ReadLine())
+        {
+            case "a":
+                Console.WriteLine($"You selected 'add': {num1 + num2}");
+                break;
+            default:
+                Console.WriteLine("No clue what to do here. Sorry!");
+                break;
+        }
+        Console.WriteLine("Thanks for playing!");
+    }
+}
